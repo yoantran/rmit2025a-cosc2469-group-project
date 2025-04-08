7f6f6aaceee02abef559081b68a55a2e361dd980
Nguyen Khang <nguyengiakhang646@gmail.com>
2025-04-08 19:53:03 +0700

    feat: Add Sudoku solver implementation with Dancing Links algorithm

---
+++ src/main/java/vn/rmit/cosc2469/Examples.java
@@ -0,0 +1,127 @@
+package vn.rmit.cosc2469;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import java.util.*;
+
+public class Examples {
+    private static int[][] fromString(String s) {
+        int[][] board = new int[9][9];
+        for (int i = 0; i < 81; i++) {
+            char c = s.charAt(i);
+            int row = i / 9;
+            int col = i % 9;
+            if (c != '.') {
+                board[row][col] = c - '0';
+            }
+        }
+        return board;
+    }
+
+    private static void printStats(List<Long> timings) {
+        long min = timings.get(0);
+        long max = timings.get(0);
+
+        long sum = 0;
+        long sqsum = 0;
+
+        for (long ll : timings) {
+            min = Math.min(min, ll);
+            max = Math.max(max, ll);
+            sum += ll;
+        }
+
+        double avg = sum / timings.size();
+
+        for (long ll : timings) {
+            sqsum += (ll - avg) * (ll - avg);
+        }
+
+        double std = Math.sqrt(sqsum / timings.size());
+
+        System.out.println("min: " + min * 1e-6);
+        System.out.println("max: " + max * 1e-6);
+        System.out.println("avg: " + avg * 1e-6);
+        System.out.println("std: " + std * 1e-6);
+    }
+
+    public static void runCoverExample() {
+        int[][] example = { { 0, 0, 1, 0, 1, 1, 0 },
+                { 1, 0, 0, 1, 0, 0, 1 },
+                { 0, 1, 1, 0, 0, 1, 0 },
+                { 1, 0, 0, 1, 0, 0, 0 },
+                { 0, 1, 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 1, 1, 0, 1 }
+        };
+
+        DancingLinks DLX = new DancingLinks(example);
+        DLX.runSolver();
+    }
+
+    public static void runSudokuExample() {
+        int[][] hardest = {
+                { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+                { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+                { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+                { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+                { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+                { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+        }; // apparently the hardest sudoku
+
+        SudokuDLX sudoku = new SudokuDLX();
+        sudoku.solve(hardest);
+    }
+
+    public static void runExample() {
+
+        String[] diffs = { "simple.txt", "easy.txt", "intermediate.txt", "expert.txt" };
+
+        BufferedReader reader = null;
+        String text = null;
+
+        for (String diff : diffs) {
+
+            String filename = "src/main/resources/" + diff;
+
+            List<Long> timings = new ArrayList<Long>();
+
+            try {
+                reader = new BufferedReader(new FileReader(filename));
+
+                while ((text = reader.readLine()) != null) {
+
+                    int[][] sudoku = fromString(text);
+
+                    SudokuDLX solver = new SudokuDLX();
+
+                    long milis = System.nanoTime();
+
+                    solver.solve(sudoku);
+
+                    long elapsed = System.nanoTime() - milis;
+
+                    timings.add(elapsed);
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("STATS: " + diff + "\n");
+            printStats(timings);
+        }
+    }
+
+    public static void main(String[] args) {
+        // runCoverExample();
+        // runSudokuExample();
+
+        runExample();
+    }
+
+}
